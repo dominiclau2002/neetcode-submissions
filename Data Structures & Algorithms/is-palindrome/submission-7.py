3a8e884d7d3c6329db6d09d2cde23419957c1eae
Lau Tze Hao, Dominic <dominic.lau.2023@scis.smu.edu.sg>
2026-09-06 21:29:35 +0800

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        #remove whitespaces and make all chars lowercase
+        s = s.lower()
+        sarr = [i for i in s if i.isalnum()]
+        s = "".join(sarr)
+
+        #initialize index
+        i1 = 0
+        i2 = len(s) - 1
+
+        print(s)
+
+        if len(s) == 0:
+            return True
+
+        for a in range(len(s)):
+        #check if index 1 == index 2, if it is not then immediately return False
+            if s[i1] != s[i2]:
+                return False
+
+            #move the pointers inwards
+            i1 += 1
+            i2 -= 1
+
+            #when the indexes meets each other, break and return True
+            if i1 == i2 or i1 > i2:
+                return True
+
+            
+
+
+
+
+            
+
+            
+
+        
